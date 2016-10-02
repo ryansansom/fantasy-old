7167619aef29f9d06e69c9374a2a9886b79c6956
Ryan Sansom <ryan.sansom@uk.tesco.com>
2016-10-02 21:35:09 +0100

    Add getPlayers function

--- routes/test-data.js
+++ routes/test-data.js
@@ -1,4 +1,5 @@
 import getLeagues from '../lib/get-leagues';
+import getPlayers from '../lib/get-players';
 import getStandings from '../lib/get-standings';
 import getWeek from '../lib/get-week';
 import { Router } from 'express';
@@ -20,6 +21,12 @@ router.get('/league-standings/:leagueType/:leagueID', function(req, res) {
   const standings = getStandings(req.params.leagueType, req.params.leagueID);
 
   return res.send(standings);
+});
+
+router.get('/players/:playerID/:week', function(req, res) {
+  const players = getPlayers(req.params.playerID, req.params.week);
+
+  return res.send(players);
 });
 
 export default router;
