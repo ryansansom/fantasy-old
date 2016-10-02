69917c5a7bb0c535179b864a2708d60c4b767efb
Ryan Sansom <ryan.sansom@uk.tesco.com>
2016-10-02 20:53:59 +0100

    Add getLeagues function

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import testData from './test-data';
 const router = Router();
 
 router.get('/', function(req, res) {
   return res.send({"hello": "test"});
 });
+
+router.get('/test', testData);
 
 export default router;
