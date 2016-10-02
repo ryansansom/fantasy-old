6da07b95080e9e6181d6eb619749db425ecb95f2
Ryan Sansom <ryan.sansom@uk.tesco.com>
2016-10-02 21:05:22 +0100

    Add getWeek function

--- routes/test-data.js
+++ routes/test-data.js
@@ -1,4 +1,5 @@
 import getLeagues from '../lib/get-leagues';
+import getWeek from '../lib/get-week';
 import { Router } from 'express';
 const router = Router();
 
@@ -6,6 +7,12 @@ router.get('/leagues', function(req, res) {
   const leagues = getLeagues('616549', 'h2h');
 
   return res.send(leagues);
+});
+
+router.get('/week', function(req, res) {
+  const week = getWeek();
+
+  return res.send(week);
 });
 
 export default router;
