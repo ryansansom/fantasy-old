4bfc60a7010cb1a6af0bb5864dfd10997a4d1553
Ryan Sansom <ryan.sansom@uk.tesco.com>
2016-10-11 23:52:04 +0100

    Converted everything to Promises

--- routes/test-data.js
+++ routes/test-data.js
@@ -9,39 +9,45 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/leagues/:type', function(req, res) {
-  const leagues = getLeagues('616549', req.params.type);
-
-  return res.send(leagues);
+  getLeagues('616549', req.params.type)
+    .then(leagues => res.send(leagues))
+    .catch(err => next(err));
 });
 
-router.get('/week', function(req, res) {
-  const week = getWeek();
-
-  return res.send(week);
+router.get('/week', function(req, res, next) {
+  return getWeek()
+    .then(week => res.send(week))
+    .catch(err => next(err));
 });
 
-router.get('/league-standings/:leagueType/:leagueID', function(req, res) {
-  const standings = getStandings(req.params.leagueType, req.params.leagueID);
-
-  return res.send(standings);
+router.get('/league-standings/:leagueType/:leagueID', function(req, res, next) {
+  getStandings(req.params.leagueType, req.params.leagueID)
+    .then(standings => res.send(standings))
+    .catch(err => next(err));
 });
 
 router.get('/players/:userID/:week', function(req, res) {
-  const players = getPlayers(req.params.userID, req.params.week);
-
-  return res.send(players);
+  getPlayers(req.params.userID, req.params.week)
+    .then(players => res.send(players))
+    .catch(err => next(err));
 });
 
 router.get('/player-points/:playerID', function(req, res) {
-  const playerPoints = getPlayerPoints(req.params.playerID);
+  getPlayerPoints(req.params.playerID)
+    .then(playerPoints => res.send(playerPoints))
+    .catch(err => next(err));
+});
 
-  return res.send(playerPoints);
+router.get('/team-points/:userID/:week', function(req, res) {
+  getTeamPoints(req.params.userID, req.params.week)
+    .then(totalPoints => res.send(totalPoints))
+    .catch(err => next(err));
 });
 
 router.get('/total-points/:userID/:week', function(req, res) {
-  const totalPoints = getTotalPoints(req.params.userID, req.params.week);
-
-  return res.send(totalPoints);
+  getTotalPoints(req.params.userID, req.params.week)
+    .then(totalPoints => res.send(totalPoints))
+    .catch(err => next(err));
 });
 
 export default router;
