9dbc0b179265c4069034f0b2417144a95c25fae1
Ryan Sansom <ryan.sansom@uk.tesco.com>
2016-10-15 11:53:28 +0100

    Converted test routes to async functions

--- routes/test-data.js
+++ routes/test-data.js
@@ -1,4 +1,4 @@
-import { getEventLive } from '../lib/fetch-data';
+import errHandler from './async-route-handler';
 import getLeagues from '../lib/get-leagues';
 import getPlayers from '../lib/get-players';
 import getPlayerPoints from '../lib/get-player-points';
@@ -9,46 +9,39 @@ import getWeek from '../lib/get-week';
 import { Router } from 'express';
 const router = Router();
 
-router.get('/leagues/:type', function(req, res) {
-  return getLeagues('616549', req.params.type)
-    .then(leagues => res.send(leagues))
-    .catch(err => next(err));
-});
+router.get('/leagues/:type', errHandler(async(req, res, next) => { // eslint-disable-line no-unused-vars
+  const leagues = await getLeagues('616549', req.params.type);
+  return res.send(leagues);
+}));
 
-router.get('/week', function(req, res, next) {
-  return getWeek()
-    .then(week => res.send(week))
-    .catch(err => next(err));
-});
+router.get('/week', errHandler(async(req, res, next) =>  { // eslint-disable-line no-unused-vars
+  const week = await getWeek();
+  return res.send(week);
+}));
 
-router.get('/league-standings/:leagueType/:leagueID', function(req, res, next) {
-  return getStandings(req.params.leagueType, req.params.leagueID)
-    .then(standings => res.send(standings))
-    .catch(err => next(err));
-});
+router.get('/league-standings/:leagueType/:leagueID', errHandler(async(req, res, next) => { // eslint-disable-line no-unused-vars
+  const standings = await getStandings(req.params.leagueType, req.params.leagueID);
+  return res.send(standings);
+}));
 
-router.get('/players/:userID/:week', function(req, res) {
-  return getPlayers(req.params.userID, req.params.week)
-    .then(players => res.send(players))
-    .catch(err => next(err));
-});
+router.get('/players/:userID/:week', errHandler(async(req, res, next) => { // eslint-disable-line no-unused-vars
+  const players = await getPlayers(req.params.userID, req.params.week);
+  return res.send(players);
+}));
 
-router.get('/player-points/:playerID', function(req, res) {
-  return getPlayerPoints([{element: req.params.playerID, multiplier: 1}, {element: 174, multiplier: 1}])
-    .then(playerPoints => res.send(playerPoints))
-    .catch(err => next(err));
-});
+router.get('/player-points/:playerID', errHandler(async(req, res, next) => { // eslint-disable-line no-unused-vars
+  const playerPoints = await getPlayerPoints([{element: req.params.playerID, multiplier: 1}, {element: 174, multiplier: 1}]);
+  return res.send(playerPoints);
+}));
 
-router.get('/team-points/:userID/:week', function(req, res) {
-  return getTeamPoints(req.params.userID, req.params.week)
-    .then(totalPoints => res.send(totalPoints))
-    .catch(err => next(err));
-});
+router.get('/team-points/:userID/:week', errHandler(async(req, res, next) => { // eslint-disable-line no-unused-vars
+  const teamPoints = await getTeamPoints(req.params.userID, req.params.week);
+  return res.send(teamPoints);
+}));
 
-router.get('/total-points/:userID/:week', function(req, res) {
-  return getTotalPoints(req.params.userID, req.params.week)
-    .then(totalPoints => res.send(totalPoints))
-    .catch(err => next(err));
-});
+router.get('/total-points/:userID/:week', errHandler(async(req, res, next) => { // eslint-disable-line no-unused-vars
+  const totalPoints = await getTotalPoints(req.params.userID, req.params.week);
+  return res.send(totalPoints);
+}));
 
 export default router;
