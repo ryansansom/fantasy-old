8faece2cba5bf7f911c1fbcdb5ee26efaaf5a5f6
Ryan Sansom <ryan.sansom@uk.tesco.com>
2016-11-13 21:12:59 +0000

    Reroute request back through application (#11)

--- components/pages/Standings/index.js
+++ components/pages/Standings/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { mockFetch } from '../../../redux/actions';
 import { mockRealAPI } from '../../mock-api';
-import getDetailedStandings from '../../../lib/get-detailed-standings';
+import { getStandings } from '../../../lib/internal-api';
 
 const pageName = 'Standings';
 
@@ -12,12 +12,12 @@ if (process.env.CLIENT_RENDER) {
 
 class Standings extends Component {
   static fetchData(dispatch, { leagueID }) {
-    return mockFetch(leagueID ? getDetailedStandings(leagueID) : mockRealAPI(), pageName, true)(dispatch);
+    return mockFetch(leagueID ? getStandings(leagueID) : mockRealAPI(), pageName, true)(dispatch);
   }
 
   componentDidMount() {
     document.title = pageName;
-    if (this.props.page !== pageName) this.props.mockFetch(this.props.params.leagueID ? getDetailedStandings(this.props.params.leagueID) : mockRealAPI(), pageName, true)
+    if (this.props.page !== pageName) this.props.mockFetch(this.props.params.leagueID ? getStandings(this.props.params.leagueID) : mockRealAPI(), pageName, true)
   }
 
   renderTable() {
@@ -54,6 +54,7 @@ class Standings extends Component {
 
   render() {
     const { standings } = this.props;
+    const refreshLinkUrl = this.props.params.leagueID ? '/standings/' + this.props.params.leagueID : '/standings';
     return (
       <div className='standings'>
         <div className="standings--header">Welcome to the new, improved view of Fantasy Premier League</div>
@@ -65,9 +66,9 @@ class Standings extends Component {
               className="refresh-results"
               onClick={e => {
                 e.preventDefault();
-                return this.props.mockFetch(this.props.params.leagueID ? getDetailedStandings(this.props.params.leagueID) : mockRealAPI(), pageName, true);
+                return this.props.mockFetch(this.props.params.leagueID ? getStandings(this.props.params.leagueID) : mockRealAPI(), pageName, true);
               }}
-              href="/standings">Refresh</a>
+              href={refreshLinkUrl}>Refresh</a>
             <div className="table-wrapper">
               {this.props.updating ? <span>Updating...</span> : this.renderTable()}
             </div>
