d74727a7b5a68e89a0fedef973157bb9e2470ed8
Ryan Sansom <ryan.sansom@uk.tesco.com>
2016-11-21 08:37:59 +0000

    Moved table into component

---
+++ components/classic-table/index.js
@@ -0,0 +1,56 @@
+import Accordion from '../accordion';
+import React, { Component, PropTypes } from 'react';
+
+class ClassicTable extends Component {
+  static propTypes = {
+    players: PropTypes.array.isRequired
+  };
+
+  render() {
+    const { players } = this.props;
+    const playerList = players
+      .sort((a, b) => (b.prevTotal + b.projectedPoints) - (a.prevTotal + a.projectedPoints))
+      .map((player, i) => {
+        const playerRow = <div>
+          <div className="col-1-of-7 table-format">{i + 1}</div>
+          <div className="col-1-of-7 table-format">{player.player_name}</div>
+          <div className="col-1-of-7 table-format">{player.prevTotal}</div>
+          <div className="col-1-of-7 table-format">{player.currentPoints}</div>
+          <div className="col-1-of-7 table-format">{player.projectedPoints}</div>
+          <div className="col-1-of-7 table-format">{player.prevTotal + player.currentPoints}</div>
+          <div className="col-1-of-7 table-format">{player.prevTotal + player.projectedPoints}</div>
+        </div>;
+
+        return (
+          <Accordion
+            tag="li"
+            key={player.entry}
+            title={player.entry.toString()}
+            header={playerRow}>
+            <div>
+              {`Team info for ${player.player_name} coming soon...`}
+            </div>
+          </Accordion>
+        )
+      });
+
+    return (
+      <div>
+        <div className="header-row">
+          <div className="col-1-of-7 table-header table-format">Position</div>
+          <div className="col-1-of-7 table-header table-format">Player</div>
+          <div className="col-1-of-7 table-header table-format">Previous Total</div>
+          <div className="col-1-of-7 table-header table-format">Current Points</div>
+          <div className="col-1-of-7 table-header table-format">Projected Points</div>
+          <div className="col-1-of-7 table-header table-format">Current Total</div>
+          <div className="col-1-of-7 table-header table-format">Projected Total</div>
+        </div>
+        <ul className="player-list">
+          {playerList}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default ClassicTable;
--- components/pages/Standings/index.js
+++ components/pages/Standings/index.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { mockFetch } from '../../../redux/actions';
 import { mockRealAPI } from '../../mock-api';
 import { getStandings } from '../../../lib/internal-api';
-import Accordion from '../../accordion';
+import ClassicTable from '../../classic-table';
 
 const pageName = 'Standings';
 
@@ -19,50 +19,6 @@ class Standings extends Component {
   componentDidMount() {
     document.title = pageName;
     if (this.props.page !== pageName) this.props.mockFetch(this.props.params.leagueID ? getStandings(this.props.params.leagueID) : mockRealAPI(), pageName, true)
-  }
-
-  renderTable() {
-    const { players } = this.props.standings;
-    const heading = <li key="header" className="header-row">
-      <div className="col-1-of-7 table-header table-format">Position</div>
-      <div className="col-1-of-7 table-header table-format">Player</div>
-      <div className="col-1-of-7 table-header table-format">Previous Total</div>
-      <div className="col-1-of-7 table-header table-format">Current Points</div>
-      <div className="col-1-of-7 table-header table-format">Projected Points</div>
-      <div className="col-1-of-7 table-header table-format">Current Total</div>
-      <div className="col-1-of-7 table-header table-format">Projected Total</div>
-    </li>;
-    const playerRows = players
-      .sort((a, b) => (b.prevTotal + b.projectedPoints) - (a.prevTotal + a.projectedPoints))
-      .map((player, i) => {
-        const playerRow = <div>
-          <div className="col-1-of-7 table-format">{i + 1}</div>
-          <div className="col-1-of-7 table-format">{player.player_name}</div>
-          <div className="col-1-of-7 table-format">{player.prevTotal}</div>
-          <div className="col-1-of-7 table-format">{player.currentPoints}</div>
-          <div className="col-1-of-7 table-format">{player.projectedPoints}</div>
-          <div className="col-1-of-7 table-format">{player.prevTotal + player.currentPoints}</div>
-          <div className="col-1-of-7 table-format">{player.prevTotal + player.projectedPoints}</div>
-        </div>;
-
-        return (
-          <Accordion
-            tag="li"
-            key={player.entry}
-            title={player.entry.toString()}
-            header={playerRow}>
-            <div>
-              {`Team info for ${player.player_name} coming soon...`}
-            </div>
-          </Accordion>
-        )
-      });
-    return (
-      <ul className="player-list">
-        {heading}
-        {playerRows}
-      </ul>
-    );
   }
 
   render() {
@@ -83,7 +39,7 @@ class Standings extends Component {
               }}
               href={refreshLinkUrl}>Refresh</a>
             <div className="table-wrapper">
-              {this.props.updating ? <span>Updating...</span> : this.renderTable()}
+              {this.props.updating ? <span>Updating...</span> : <ClassicTable players={standings.players} />}
             </div>
           </div>
         </div>
