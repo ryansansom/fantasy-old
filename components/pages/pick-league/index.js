0f245c73a633062cf0ed699caebe716c76148104
Ryan Sansom <ryan.sansom@uk.tesco.com>
2016-12-17 14:11:36 +0000

    Build out pick league page with some initial context

--- components/pages/pick-league/index.js
+++ components/pages/pick-league/index.js
@@ -21,14 +21,41 @@ class PickLeague extends Component {
     if (this.props.page !== pageName) this.props.leagueList(getLeagueList(), pageName);
   }
 
+  renderLeaguesList() {
+    const mainList = this.props.leaguesList.map(league => {
+      return (
+        <li key={league.leagueId}>
+          <Link to={"/standings/" + league.leagueId}>{league.leagueName}</Link>
+        </li>
+      );
+    });
+
+    if (process.env.NODE_ENV !== 'production') mainList.push(
+      <li key='dummy'>
+        <Link to={"/standings/"}>Sample League Data</Link>
+      </li>
+    );
+
+    return (
+      <ul className="classic-leagues--list">
+        {mainList}
+      </ul>
+    );
+  }
+
   render() {
     return this.props.updating ?
       <span>Updating</span> // Put inside page wrapper
       :
       <div className='pick-league'>
         <StandardLayout title="Welcome to the new, improved view of Fantasy Premier League">
-          <Link to='/standings'>STANDINGS</Link>
-          <span>{`I am the pick league page`}</span>
+          <h1>League Lists</h1>
+
+          <h2>Classic Leagues</h2>
+          {this.props.leaguesList && this.renderLeaguesList()}
+
+          <h2>Head-to-head Leagues</h2>
+          <p>... Coming soon!</p>
         </StandardLayout>
       </div>;
   }
