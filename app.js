f8043fbab73b40c941806a60db7698f64ee76341
Ryan Sansom <ryan.sansom@uk.tesco.com>
2018-01-17 17:02:48 +0000

    Rename function and remove redundant code

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import routes from './routes';
 import { notFound, devError, prodError } from './routes/error-handler';
 
 const app = express();
+const errorHandler = app.get('env') === 'development' ? devError : prodError;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,17 +18,7 @@ app.use('/', routes);
 // catch 404 and forward to error handler
 app.use(notFound);
 
-// error handlers
+// error handler
+app.use(errorHandler);
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(devError);
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(prodError);
-
-
-module.exports = app;
+export default app;
